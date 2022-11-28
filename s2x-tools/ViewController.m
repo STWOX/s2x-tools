88ce7bd3ad6e479201c11db016497cdb96f0773e
niu2x <niu2x@icloud.com>
2022-11-28 20:17:59 +0800

    add poem

--- s2x-tools/ViewController.m
+++ s2x-tools/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import "../masonry/Masonry.h"
 
 @interface ViewController ()
 
@@ -16,6 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+
+    UILabel *content =
+        [[[UILabel alloc] initWithFrame:CGRectMake(0, 0, 0, 0)] autorelease];
+    [self.view addSubview:content];
+    [content setText:@"春眠不觉晓\n处处闻啼鸟\n夜来风雨声\n花落知多少"];
+    content.numberOfLines = 0;
+    content.textAlignment = NSTextAlignmentCenter;
+
+    [content mas_makeConstraints:^(MASConstraintMaker *make) {
+      make.left.equalTo(self.view.mas_left);
+      make.right.equalTo(self.view.mas_right);
+      make.top.equalTo(self.view.mas_top);
+      make.bottom.equalTo(self.view.mas_bottom);
+    }];
+
+    [content retain];
 }
 
 
